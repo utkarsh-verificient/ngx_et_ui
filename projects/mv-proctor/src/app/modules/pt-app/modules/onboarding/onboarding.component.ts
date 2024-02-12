d7dcb2149ff697932a36e2769479d5f898384e88
Utkarsh Pathak <utkarsh@verificient.com>
2024-02-12 13:53:03 +0530

    created onboarding module

---
+++ projects/mv-proctor/src/app/modules/pt-app/modules/onboarding/onboarding.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'vt-onboarding',
+  templateUrl: './onboarding.component.html',
+  styleUrl: './onboarding.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class OnboardingComponent {
+
+}
