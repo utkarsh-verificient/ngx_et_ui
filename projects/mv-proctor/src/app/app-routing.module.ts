68d841c4a726c1cfe2a117b57d83a0f42996b225
Utkarsh Pathak <utkarsh@verificient.com>
2024-02-12 12:37:18 +0530

    updated routing module

--- projects/mv-proctor/src/app/app-routing.module.ts
+++ projects/mv-proctor/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout/layout.component';
+
+const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'pt' },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'pt',
+                loadChildren: () => import('./modules').then((m) => m.PtAppModule),
+            },
+            {
+                path: 'br',
+                loadChildren: () => import('./modules').then((m) => m.PtBrowserModule),
+            },
+        ],
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ projects/mv-proctor/src/app/modules/index.ts
@@ -0,0 +1,2 @@
+export * from './pt-app/pt-app.module';
+export * from './pt-browser/pt-browser.module';
