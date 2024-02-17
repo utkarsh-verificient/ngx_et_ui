ca9612059effd73aec5b14236693cbbd0fd2d279
Utkarsh Pathak <utkarsh@verificient.com>
2024-02-17 23:31:48 +0530

    updated browser layout

--- projects/mv-proctor/src/app/layout/layouts/browser-layout/browser-layout.module.ts
+++ projects/mv-proctor/src/app/layout/layouts/browser-layout/browser-layout.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserLayoutComponent } from './browser-layout.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [BrowserLayoutComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule
+  ],
+  exports: [BrowserLayoutComponent]
 })
 export class BrowserLayoutModule { }
